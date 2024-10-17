4e9527383ff7902106783a5d242a1eac86eca893
manasmani <manasmani176@gmail.com>
2024-10-17 14:40:23 +0530

    update

---
+++ fourth.js
@@ -0,0 +1,8 @@
+import {add, subtract,PI} from "./mathUtlis.js";
+
+import * as jadoo from "./mathUtlis.js";
+
+console.log("Add: ", check.add(5,3));
+console.log("Subtract: ", check.subtract(5,3));
+console.log("PI: ",check.PI);
+consolelog("Multiply: ",check.multiply(5,3));
---
+++ third.js
@@ -0,0 +1,17 @@
+export function add(x, y){
+    return x + y;
+}
+export function subtract (x,y){
+    return x-y;
+}
+export const PI = 3.14159;
+
+export default function multiply (x, y){
+    return x * y;
+}
+module.export = {
+    add,
+    subtract,
+    PI,
+    multiply,
+}
